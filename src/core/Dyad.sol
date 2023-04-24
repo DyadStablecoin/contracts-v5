ef2afb07e7db19858f4862e0562e18bd0294a01a
shafu.eth <selfouly@gmail.com>
2023-04-24 23:12:50 +0200

    vault manager

--- src/core/Dyad.sol
+++ src/core/Dyad.sol
@@ -3,15 +3,38 @@ pragma solidity =0.8.17;
 
 import {Owned} from "@solmate/src/auth/Owned.sol";
 import {ERC20} from "@solmate/src/tokens/ERC20.sol";
+import {VaultsManager} from "../composing/VaultsManager.sol";
 
 contract Dyad is ERC20, Owned {
+  VaultsManager public immutable vaultsManager;
+
+  error Unauthorized();
+
   constructor(
     string memory name, 
     string memory symbol, 
-    address owner
+    address owner, 
+    address _vaultsManager
   ) ERC20(name, symbol, 18) 
-    Owned(owner) {}
+    Owned(owner) {
+      vaultsManager = VaultsManager(_vaultsManager);
+  }
 
-  function mint(address to,   uint amount) external onlyOwner {_mint(to,   amount);}
-  function burn(address from, uint amount) external onlyOwner {_burn(from, amount);}
+  function mint(
+      address to,
+      uint    amount,
+      address vault
+  ) external {
+      if (vaultsManager.vaultVotes(vault) < 40) revert Unauthorized();
+      _mint(to, amount);
+  }
+
+  function burn(
+      address from,
+      uint    amount, 
+      address vault
+  ) external {
+      if (vaultsManager.vaultVotes(vault) < 40) revert Unauthorized();
+      _burn(from, amount);
+  }
 }
